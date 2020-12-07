5b8919d392367217b408f9285c2841c3ad4e02c4
Matthew Ross <mross20@myseneca.ca>
2020-12-07 10:48:20 -0800

    Added About page to feature repo contributors
    Included button to About page when logged in
    
    Fixed formatting on About page

--- contestreminder/contestreminder/urls.py
+++ contestreminder/contestreminder/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
+from users.views import contribute
+
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('about/', contribute, name='about'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
--- contestreminder/users/views.py
+++ contestreminder/users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 from django.urls import reverse_lazy
@@ -10,3 +11,10 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+def contribute(request):
+    contributors = {}
+    url = 'https://api.github.com/repos/codestromer/ContestReminder/contributors'
+    response = requests.get(url)
+    contributors = response.json()
+    return render(request, 'about.html', {'contributors': contributors})
