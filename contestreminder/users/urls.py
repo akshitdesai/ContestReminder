68a82ced8a29b5e2ba1642e5e10c6fb628aefb28
Matthew Ross <mross20@myseneca.ca>
2020-12-12 14:23:18 -0800

    Profile and Delete Page functional
    Added form to delete.html to allow POST and delete of account
    
    Removed unused code

--- contestreminder/users/urls.py
+++ contestreminder/users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import SignUpView
+from django.contrib.auth.decorators import login_required
+from .views import SignUpView, ProfileView, DeleteUserView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('profile/', login_required(ProfileView.as_view()), name='profile'),
+    path('delete/', login_required(DeleteUserView.as_view()), name='delete'),
 ]
